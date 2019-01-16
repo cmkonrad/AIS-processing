6fee3ce50d87107e0220a17c8e19d9257d218faa
cmkonrad <45180861+cmkonrad@users.noreply.github.com>
2019-01-16 16:56:43 -0400

    remove personal info
    update example directories

--- ExampleSourceCode/SelectAreaDates_example.R
+++ ExampleSourceCode/SelectAreaDates_example.R
@@ -1,19 +1,26 @@
 
 require(mgcv)  #package that contains the 'in.out' function
 
-source("C:/Users/KONRADC/Desktop/RCode/R_Functions/GetPointsInArea_SpecifyRegion.R")
+source("C:/Users/CMKONRAD/Desktop/RCode/R_Functions/GetPointsInArea_SpecifyRegion.R") #to allow R to use this custom function
 
 
-filelist <- list.files(path = "E:/ESI_project/Dynamic_Data/FromDiana/Dynamic", pattern = "2017-06")  # this lists all files in June 2017
-filepath <- file.path("E:/ESI_project/Dynamic_Data/FromDiana/Dynamic", filelist)
+filelist <- list.files(path = "E:/AIS/Dynamic_Data", pattern = "2017-06")  # pattern example lists all files in June 2017
+filepath <- file.path("E:/AIS/Dynamic_Data", filelist)
 
 
-outfilepath <- "E:/ForAngelia"            # specify directory where outfile should go
+outfilepath <- "E:/AIS_Subset"            # specify directory where outfile should go
 outfilename = "CCG_gulf_June2017.csv"     # rename as needed
 
+# latitude and longitude coordinates for corners of area of interest
+left <- -67.9
+right <- -54.8
+top <- 47.9
+bot <- 39.9
 
-polycorners <- c(-66, 44.5, -59, 44.5, -59, 51.5, -66, 51.5)  # latitude and longitude coordinates for corners of area of interest
-
+polycorners <- c(left, top, 
+                 left, bot,
+                 right, bot,
+                 right, top)
 
 region = c("M", "N", "Q", "U")    # options: "C", "M", "N", "P", "Q", "U", or can specify "All"; for satellite data, specify "All"
 
